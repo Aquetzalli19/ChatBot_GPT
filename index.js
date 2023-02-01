22354e949c474ab23750f9df5a41e8bd99cb9c05
Chava <schavarria13@alumnos.uaq.mx>
2023-02-01 14:45:40 -0600

    Pruebas

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ import request from "request";
 
 
 //llaves de chat GPT
-import fetch from "node-fetch";
-const API_KEY = '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';
+const API_KEY = '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';
 const MODEL_ENGINE = 'text-davinci-003';
 
 //Variables servidor
@@ -78,10 +77,35 @@ app.get('/webhook', (req, res) => {
 function handleMessage(sender_psid, received_message){
     let response;
 
+    async function generateText(prompt) {
+        try {
+          const response = await fetch(`https://api.openai.com/v1/engines/${MODEL_ENGINE}/completions`, {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${API_KEY}`
+            },
+            body: JSON.stringify({
+              prompt: prompt,
+              max_tokens: 1024,
+              n: 1,
+              stop: null,
+              temperature: 0.5
+            })
+          });
+          const data = await response.json();
+          return data.choices[0].text;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      
+      // Usage
+      const prompt = received_message.text;
 
     if(received_message.text){
         response = {
-            "text": `Tu mensaje es : ${received_message.text}`
+            "text": generateText(prompt).then(output => output)
         };
     }
 
