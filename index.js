d610f6fff28c8e75bc7666962f3e1c7b167072c2
Chava <schavarria13@alumnos.uaq.mx>
2023-02-02 12:58:42 -0600

    Correciones de gpt

--- index.js
+++ index.js
@@ -78,43 +78,42 @@ app.get('/webhook', (req, res) => {
 function handleMessage(sender_psid, received_message){
     let response;
 
-    async function generateText(prompt) {
-        try {
-        const response = await axios({
-        method: 'post',
-        url: `https://api.openai.com/v1/engines/${MODEL_ENGINE}/completions`,
-        headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_KEY}`
-        },
-        data: {
-        prompt: prompt,
-        max_tokens: 1024,
-        n: 1,
-        stop: null,
-        temperature: 0.5
-        }
-        });
-        return response.data.choices[0].text;
-        } catch (error) {
-        console.error(error);
-        }
-        }
-        
-        // Usage
-        const prompt = received_message.text;
+async function generateText(prompt) {
+    try {
+    const response = await axios({
+    method: 'post',
+    url: `https://api.openai.com/v1/engines/${MODEL_ENGINE}/completions`,
+    headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${API_KEY}`
+    },
+    data: {
+    prompt: prompt,
+    max_tokens: 1024,
+    n: 1, stop: null,
+    temperature: 0.5
+    }
+    });
+    return response.data.choices[0].text;
+    } catch (error) {
+    console.error(error);
+    }
+    }
+    
+    // Usage
+    const prompt = received_message.text;
         
     if(received_message.text){
-        let gptResponse =  generateText(prompt).then(output => output); 
-        response = {
-            "text": gptResponse
-        };
+        generateText(prompt).then(output => {
+            response = {
+                "text": output
+            };
+            callSendApi(sender_psid, response);
+            console.log(response);
+        }).catch(error => {
+            console.error(error);
+        });
     }
-
-    setTimeout(()=>{
-        callSendApi(sender_psid, response);
-        console.log(response);
-    }, 2000)
     
 }
 
